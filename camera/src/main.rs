538b97756632bb403d3013e351884b631c1c1c75
Stephen Schwink <stephen.schwink@gmail.com>
2025-09-22 13:17:21 -0700

    List USB devices

--- camera/src/main.rs
+++ camera/src/main.rs
@@ -1,3 +1,40 @@
+use rusb::{LibraryVersion, version};
+
 fn main() {
     println!("Hello, world!");
+
+    let rusb_version = rusb::version();
+    println!(
+        "rusb version: {}.{}",
+        rusb_version.major(),
+        rusb_version.minor(),
+    );
+
+    let devices = rusb::devices();
+    match devices {
+        Ok(devices) => {
+            println!("Found {} devices", devices.len());
+            for device in devices.iter() {
+                let device_desc = device.device_descriptor();
+                match device_desc {
+                    Ok(desc) => {
+                        println!(
+                            "{:?}\nBus {:03} Device {:03} ID {:04x}:{:04x}\n",
+                            desc,
+                            device.bus_number(),
+                            device.address(),
+                            desc.vendor_id(),
+                            desc.product_id()
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("Error getting device descriptor: {}", e);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("Error getting device list: {}", e);
+        }
+    }
 }
