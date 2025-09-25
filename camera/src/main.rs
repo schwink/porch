9be02b86ba16aa1565e5e29962fb5da68277a946
Stephen Schwink <stephen.schwink@gmail.com>
2025-09-25 15:46:36 -0700

    Remove output of available camera modes

--- camera/src/main.rs
+++ camera/src/main.rs
@@ -4,46 +4,6 @@ mod camera;
 
 #[tokio::main]
 async fn main() {
-    let uvc_context = uvc::Context::new().expect("Could not get uvc context");
-
-    let uvc_devices = uvc_context
-        .devices()
-        .expect("Could not enumerate uvc devices");
-    uvc_devices.for_each(|device| {
-        let desc = device
-            .description()
-            .expect("Could not get device descriptor");
-        println!(
-            "UVC Device: Bus {:03}, Device Address {:03}, Vendor 0x{:04x} ({}), Product ID 0x{:04x} ({}), Serial Number: {}",
-            device.bus_number(),
-            device.device_address(),
-            desc.vendor_id,
-            desc.manufacturer.unwrap_or("Unknown".to_string()),
-            desc.product_id,
-            desc.product.unwrap_or("Unknown".to_string()),
-            desc.serial_number.unwrap_or("Unknown".to_string()),
-        );
-
-        let handle = device.open().expect("Could not open device");
-
-        let formats = handle.supported_formats();
-        formats.for_each(|format| {
-            println!(
-                "Format: subtype:{:?}",
-                    format.subtype(),
-            );
-            let frame_formats = format.supported_formats();
-            frame_formats.for_each(|frame_format| {
-                println!(
-                    "Frame Format: w:{} h:{} fps:{:?}",
-                        frame_format.width(),
-                        frame_format.height(),
-                        frame_format.intervals().into_iter().map(|d| 10_000_000 / d).collect::<Vec<u32>>()
-                );
-            });
-        });
-    });
-
     let camera_service = camera::CameraService::new();
 
     let handle = tokio::spawn(async move {
