84f28a9ea4345cbe52d86785f706d169a068bbac
Stephen Schwink <stephen.schwink@gmail.com>
2025-10-09 12:34:26 -0700

    Limit tokio threads to 2
    Description:
    The camera device has four cores. We should leave an uncontested one for the `stream_callback` that receives frames from UVC.

--- camera/src/main.rs
+++ camera/src/main.rs
@@ -32,7 +32,7 @@ struct Cli {
     start_watching_immediately: bool,
 }
 
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread", worker_threads = 2)]
 async fn main() {
     let cli = Cli::parse();
 
